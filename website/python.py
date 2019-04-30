655dd6e9bbcd33ef6e31d3583582bf88a67ecbcd
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-04-30 22:20:50 +1200

    Index page nearly finished
    Created with JavaScript in mind. May need to make changes when using bottle because bottle us poo poo

--- website/python.py
+++ website/python.py
@@ -9,4 +9,12 @@ from itertools import count
 def index():
     pass
 
+@route('/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='Css/')
+
+@route('/Images/<filename>')
+def upload_image(filename):
+    return static_file(filename, root='Images/')
+
 run(host='0.0.0.0', port = 399, reloader = True, debug = True)
