60630014085a2ec5b24dcc98fc1172ab6ca55871
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-05-08 15:11:20 +1200

    Add files via upload

--- website/python.py
+++ website/python.py
@@ -1,18 +1,19 @@
 from bottle import run, route, view, get, post, request, template, static_file
 
 class Bro:
-    def __init__(self, name, description, img, num):
-        self.index = num
+    def __init__(self, name, description, img):
         self.name = name
         self.description = description
         self.img = img
        
 bros = [
-    Bro("Tom","Ble ble ble","tom.jpg","2"),
-    Bro("Jerry","Bla bla bla","jerry.jpg","1"),
-    Bro("Moses","Ble ble ble","jerry.jpg","2"),
-    Bro("John","Ble ble ble","john.jpg","2"),
-    Bro("Dom","Ble ble ble","jerry.jpg","2")
+    Bro("Tom","Generic british boi. Nice and smart so don't hire him if you don't want to feel bad about your IQ.","tom.jpg"),
+    Bro("Jerry","Good boi, will definatly tell us everything you did wrong. Cops might like this one.","jerry.jpg"),
+    Bro("Moses","Slightly retarded, will do anything for you. Makes you feel better about your problems. Depressed people and bullies may want.","moses.jpg"),
+    Bro("John","Goes to the gym.","john.jpg"),
+    Bro("Liam","Will serinade. Has guitar, good boi. Ideal for girls.","liam.jpg"),
+    Bro("Fox","Will sit and play nintendo with you for as long as you like. Presence makes you feel good. Introvertes love.","laimf.jpg"),
+    Bro("Dom","Will make you feel geneticaly inferior. Has good beauty products. Gays would buy again.","dom.jpg")
 ]
 
 
