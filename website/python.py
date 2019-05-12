1dc1b623ccb9135151654247ef6478ddfcb4d3d2
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-05-13 11:51:46 +1200

    Add files via upload

--- website/python.py
+++ website/python.py
@@ -1,19 +1,20 @@
 from bottle import run, route, view, get, post, request, template, static_file
 
 class Bro:
-    def __init__(self, name, description, img):
+    def __init__(self, name, description, img, stock):
         self.name = name
         self.description = description
         self.img = img
+        self.stock = stock
        
 bros = [
-    Bro("Tom","Generic british boi. Nice and smart so don't hire him if you don't want to feel bad about your IQ.","tom.jpg"),
-    Bro("Jerry","Good boi, will definatly tell us everything you did wrong. Cops might like this one.","jerry.jpg"),
-    Bro("Moses","Slightly retarded, will do anything for you. Makes you feel better about your problems. Depressed people and bullies may want.","moses.jpg"),
-    Bro("John","Goes to the gym.","john.jpg"),
-    Bro("Liam","Will serinade. Has guitar, good boi. Ideal for girls.","liam.jpg"),
-    Bro("Fox","Will sit and play nintendo with you for as long as you like. Presence makes you feel good. Introvertes love.","laimf.jpg"),
-    Bro("Dom","Will make you feel geneticaly inferior. Has good beauty products. Gays would buy again.","dom.jpg")
+    Bro("Tom","Generic british boi. Nice and smart so don't hire him if you don't want to feel bad about your IQ.","tom.jpg", True),
+    Bro("Jerry","Good boi, will definatly tell us everything you did wrong. Cops might like this one.","jerry.jpg", True),
+    Bro("Moses","Slightly retarded, will do anything for you. Makes you feel better about your problems. Depressed people and bullies may want.","moses.jpg", True),
+    Bro("John","Goes to the gym.","john.jpg", True),
+    Bro("Liam","Will serinade. Has guitar, good boi. Ideal for girls.","liam.jpg", True),
+    Bro("Fox","Will sit and play nintendo with you for as long as you like. Presence makes you feel good. Introvertes love.","laimf.jpg", True),
+    Bro("Dom","Will make you feel geneticaly inferior. Has good beauty products. Gays would buy again.","dom.jpg", False)
 ]
 
 
