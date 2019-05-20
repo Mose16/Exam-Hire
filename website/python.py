de15be3615ba0ff70c0a8ad0737129d20152562c
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-05-21 10:31:35 +1200

    Add files via upload

--- website/python.py
+++ website/python.py
@@ -10,8 +10,24 @@ class Bro:
         self.cost = cost
         self.booked_details = booked_details
        
+       
+months = {
+    'January' : 1,
+    'Febuary' : 2,
+    'March' : 3,
+    'April' : 4,
+    'May' : 5,
+    'June' : 6,
+    'July' : 7,
+    'August' : 8,
+    'September' : 9, 
+    'October' : 10,
+    'November' : 11,
+    'December' : 12
+}
+        
 bros = [
-    Bro("Tom","Generic british boi. Nice and smart so don't hire him if you don't want to feel bad about your IQ.","tom.jpg", 70, True),
+    Bro("Tom","Generic british boi. Nice and smart so don't hire him if you don't want to feel bad about your IQ. Will colonise only if it brings glory to his queen.","tom.jpg", 970, True),
     Bro("Jerry","Good boi, will definatly tell us everything you did wrong. Cops might like this one.","jerry.jpg", 90, True),
     Bro("Moses","Slightly retarded, will do anything for you. Makes you feel better about your problems. Depressed people and bullies may want.", "moses.jpg", 20, True),
     Bro("John","Goes to the gym.","john.jpg", 100, True),
@@ -58,17 +74,35 @@ def purchase_success():
     Lname = request.forms.get("last_name")
     date_ = request.forms.get("date")
     
+    date_alt = date_.split(" ")
+    date_alt[1] = date_alt[1].strip(",")
+    
     curr_date = datetime.now()
-    current_bro.booked_details = [Fname, Lname, str(curr_date.strftime("%B")) + " " + str(curr_date.day) + ", " + str(curr_date.year), date_]
-    d0 = date(2008, 8, 18)
-    d1 = date(2008, 9, 26)
+    d0 = date(curr_date.year, curr_date.month, curr_date.day)
+    d1 = date(int(date_alt[2]), int(months[date_alt[0]]), int(date_alt[1]))
     delta = d1 - d0
     total_cost = current_bro.cost * delta.days
+    total_cost = str(total_cost)
+    current_bro.booked_details = [Fname, Lname, str(curr_date.strftime("%B")) + " " + str(curr_date.day) + ", " + str(curr_date.year), date_, total_cost]
+    return dict(bro = current_bro)
+
+#Purchase page
+@route('/application.html')
+@view('application.html')
+def application():
+    pass
+
+#Purchase_success page
+@route('/application_success', method = "POST")
+@view('application_success')
+def application_success():
+    Fname = request.forms.get("first_name")
+    Lname = request.forms.get("last_name")
+    description = request.forms.get("description")
+    cost = int(request.forms.get("cost"))
     
-    data = dict(bro = current_bro)
-    data['cost'] = str(total_cost)
-    return data
-    
+    bros.append(Bro(Fname, description, "empty.jpg", cost, True))
+    return dict(bro = bros[-1])
 
 ##Static files###
 #Images
